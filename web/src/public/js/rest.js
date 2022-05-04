0290a51396261c81a8a781c93d51380de84eb7a1
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-04 12:41:56 -0700

    add gps map

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -3,6 +3,7 @@ var pir = document.getElementById("pir")
 var acc = document.getElementById("acc")
 var latitude = document.getElementById("lat")
 var longitude = document.getElementById("long")
+var gpsdisplay = document.getElementById("gpsdisplay")
 var stream  = setInterval(function() {}, 1000);
 
 function log_in() {
@@ -44,7 +45,7 @@ function stream_data(username,password) {
                 acc.innerHTML = "Accelerations: " + response['ax'] + ", " + response['ay'] + ", " + response['az'];
                 latitude.innerHTML = "Latitude: " + response['lat'];
                 longitude.innerHTML = "Longitude: " + response['long'];
-
+                gpsdisplay.innerHTML = " <script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>"
 
             })
 
