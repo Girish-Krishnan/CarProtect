424bd32d020c05e7939197f25ce14d4b22dd686e
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-08 19:38:42 -0700

    fix js

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -43,8 +43,10 @@ function stream_data(username,password) {
                 }
 
                 acc.innerHTML = "Accelerations: " + response['ax'] + ", " + response['ay'] + ", " + response['az'];
+                if(response['lat'] != "0.000000") {
                 latitude.innerHTML = "Latitude: " + response['lat'];
                 longitude.innerHTML = "Longitude: " + response['long'];
+                }
                 gpsdisplay.innerHTML = " <script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>"
 
             })
