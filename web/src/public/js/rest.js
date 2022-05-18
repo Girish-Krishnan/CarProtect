ddc2edf5021c249efabcdbcfa5be4cac6af9aa7b
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-18 12:25:44 -0700

    improve UI + map functionality

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -39,7 +39,7 @@ function stream_data(username,password) {
                     pir_2.innerHTML = "<img src=\'good.png\'>";
                 }
 
-                if(response['vibration'] > "4") {
+                if(response['vibration'] == "4" | response['vibration'] == "5" | response['vibration'] == "6") {
                     vibration_1.innerHTML = "<img src=\'vib_1.gif\'>";
                     vibration_2.innerHTML = "<img src=\'warning.png\'>";
                 }
@@ -55,7 +55,7 @@ function stream_data(username,password) {
                 // latitude.innerHTML = "Latitude: " + response['lat'];
                 // longitude.innerHTML = "Longitude: " + response['long'];
                 
-                gpsdisplay.innerHTML = " <script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>"
+                gpsdisplay.innerHTML = "<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>";
 
             })
 
