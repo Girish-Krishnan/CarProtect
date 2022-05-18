2086024ca62735af888880d28cc8fe6cfd1afcdb
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-18 11:57:57 -0700

    modify UI

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -1,6 +1,8 @@
-var vibration = document.getElementById("vib")
-var pir = document.getElementById("pir")
-var acc = document.getElementById("acc")
+var vibration_1 = document.getElementById("vib_1")
+var vibration_2 = document.getElementById("vib_2")
+var pir_1 = document.getElementById("people_1")
+var pir_2 = document.getElementById("people_2")
+
 var latitude = document.getElementById("lat")
 var longitude = document.getElementById("long")
 var gpsdisplay = document.getElementById("gpsdisplay")
@@ -29,19 +31,30 @@ function stream_data(username,password) {
             .then(response => response.json())
             .then(function(response) {
                 if(response['pir'] == "1") {
-                    pir.innerHTML = "Motion of a Person Nearby: Yes";
+                    pir_1.innerHTML = "<img src=\'person_1.png\'>";
+                    pir_2.innerHTML = "<img src=\'warning.png\'>";
                 }
                 else {
-                    pir.innerHTML = "Motion of a Person Nearby: No";
+                    pir_1.innerHTML = "<img src=\'person_0.png\'>";
+                    pir_2.innerHTML = "<img src=\'good.png\'>";
                 }
 
-                vibration.innerHTML = "Vibrations: " + response['vib'];
-            
-                acc.innerHTML = "Accelerations: " + response['ax'] + ", " + response['ay'] + ", " + response['az'];
-                if(response['lat'] != "0.000000") {
-                latitude.innerHTML = "Latitude: " + response['lat'];
-                longitude.innerHTML = "Longitude: " + response['long'];
+                if(response['vibration'] > "4") {
+                    vibration_1.innerHTML = "<img src=\'vib_1.gif\'>";
+                    vibration_2.innerHTML = "<img src=\'warning.png\'>";
                 }
+
+                else {
+                    vibration_1.innerHTML = "<img src=\'vib_0.gif\'>";
+                    vibration_2.innerHTML = "<img src=\'good.png\'>";
+                }
+
+              //  vibration.innerHTML = "Vibrations: " + response['vib'];
+               // acc.innerHTML = "Accelerations: " + response['ax'] + ", " + response['ay'] + ", " + response['az'];
+            
+                // latitude.innerHTML = "Latitude: " + response['lat'];
+                // longitude.innerHTML = "Longitude: " + response['long'];
+                
                 gpsdisplay.innerHTML = " <script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>"
 
             })
