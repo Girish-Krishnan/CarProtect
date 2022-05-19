53537dbaf39cc64a54c45a5012f3779f5e4469d2
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-18 17:47:15 -0700

    improved UI and fixed app

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -55,7 +55,8 @@ function stream_data(username,password) {
                 // latitude.innerHTML = "Latitude: " + response['lat'];
                 // longitude.innerHTML = "Longitude: " + response['long'];
                 
-                gpsdisplay.innerHTML = "<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>";
+               // gpsdisplay.innerHTML = "<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><div id=\"map\" style=\"width: 400px; height: 300px\"></div><script type=\"text/javascript\"> var myOptions = {zoom: 8,center: new google.maps.LatLng(" + response['lat'] + "," + response['long'] + "),mapTypeId: google.maps.MapTypeId.ROADMAP};var map = new google.maps.Map(document.getElementById(\"map\"), myOptions);</script>";
+                gpsdisplay.innerHTML = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3338.446028121861!2d-117.24541168487721!3d33.20240118084561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc7698bdca6ba3%3A0x91f39ef45a4f86dd!2s100%20Main%20St%2C%20Vista%2C%20CA%2092083!5e0!3m2!1sen!2sus!4v1652409686503!5m2!1sen!2sus\" width=\"300\" height=\"500\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>";
 
             })
 
