894f7892e77e51132c10a1a3c195b1a621ec2df2
Girish Krishnan <gikrishnan@ucsd.edu>
2022-06-04 17:12:05 -0700

    map iframe

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -88,6 +88,25 @@ function stream_data(username,password) {
                 
                 image_live.src = image_live.src.split("?")[0] + "?" + new Date().getTime();
                 image_live.innerHTML = "";
+
+                // Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("gpsdisplay"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+  
             })
 
     },500);
