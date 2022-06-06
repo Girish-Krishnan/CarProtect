1b481252f320f952cf2bab0a8cebbcf3854ec7c0
Girish Krishnan <gikrishnan@ucsd.edu>
2022-06-05 18:07:51 -0700

    vib added

--- web/src/public/js/rest.js
+++ web/src/public/js/rest.js
@@ -67,7 +67,7 @@ function stream_data(username,password) {
                     pir_2.src = "good.png".split("?")[0] + "?" + new Date().getTime();
                 }
 
-                if(response['vibration'] == 2  | response['vibration'] == 3 | response['vibration'] == 4 | response['vibration'] == 5 | response['vibration'] == 6 ) {
+                if(response['vib'] == 2  | response['vib'] == 3 | response['vib'] == 4 | response['vib'] == 5 | response['vib'] == 6 ) {
                     vibration_1.src = "vib_1.gif".split("?")[0] + "?" + new Date().getTime();
                     vibration_2.src = "warning.png".split("?")[0] + "?" + new Date().getTime();
                 }
