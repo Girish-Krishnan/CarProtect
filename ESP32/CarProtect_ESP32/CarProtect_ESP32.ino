8b6dada035fbdae018df0e80ba65d4805941a054
Girish Krishnan <gikrishnan@ucsd.edu>
2022-06-06 22:50:38 -0700

    Uploaded Arduino Code + Written Some Instructions in README

---
+++ ESP32/CarProtect_ESP32/CarProtect_ESP32.ino
@@ -0,0 +1,218 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <SoftwareSerial.h>
+//#include <TinyGPS.h>
+//SoftwareSerial gpsSerial(32,35);//rx,tx
+//TinyGPS gps; // create gps object
+int ax,ay,az;
+String latitude = "32.8755662";
+String longitude = "-117.2323252"; 
+const int buzzerPin = 19;
+const int pirPin = 4;
+const int vibPin = 18;
+const int axPin = 2;
+const int ayPin = 12;
+const int azPin = 15;
+
+int vibReadings[10] = {0,0,0,0,0,0,0,0,0,0};
+int i = 0;
+int changesVib[9] = {0,0,0,0,0,0,0,0,0};
+
+unsigned long timeStart = 0;
+int pir = 0;
+int vib = 0;
+
+const char* ssid = "io";
+const char* password = "girishkrishnan";
+
+String esp32_username = "myesp32_1234";
+String esp32_password = "thisisanesp32";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "http://164.92.89.132/unlock/" + esp32_username + "/" + esp32_password + "/";
+
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+
+
+void setup() {
+  pinMode(buzzerPin,OUTPUT);
+  pinMode(pirPin,INPUT);
+  pinMode(vibPin,INPUT);
+  setupAccelSensor();
+//  setupGPSSensor();
+  Serial.begin(115200); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+
+}
+
+void loop() {
+
+  //readGPSData();
+  readAccelSensor();
+  pir = digitalRead(pirPin);
+  vib = !digitalRead(vibPin);
+   vibReadings[i] = vib;
+    changesVib[i] = abs(vibReadings[i+1]-vibReadings[i]);
+    i++;
+    if (i == 10) {i = 0;}
+
+    int sum = 0;
+    for(int j = 0; j < 9; j++) {
+      sum += changesVib[j];
+    }
+
+  Serial.println("!!! Sum:  " + String(sum));
+  readAccelSensor();
+  Serial.println(String(ax));
+  String serverName2 =  "http://164.92.89.132/send_data/" + esp32_username + "/" + esp32_password + "/" + String(pir) + "/" + String(sum) + "/" + String(ax) +  "/" + String(ay) +  "/" + String(az) + "/" + latitude + "/" + longitude + "/";
+  Serial.println(serverName2);
+  delay(50);
+  if(sum > 3) {
+    digitalWrite(buzzerPin, HIGH);
+  }
+  else {
+    digitalWrite(buzzerPin,LOW);
+  }
+
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+//      String serverPath = serverName;
+//      
+//      // Your Domain name with URL path or IP address with path
+//      http.begin(serverPath.c_str());
+//      
+//      // Send HTTP GET request
+//      int httpResponseCode = http.GET();
+//      
+//      if (httpResponseCode>0) {
+//        Serial.print("HTTP Response code: ");
+//        Serial.println(httpResponseCode);
+//        String payload = http.getString();
+//        Serial.println(payload);
+//      }
+//      else {
+//        Serial.print("Error code: ");
+//        Serial.println(httpResponseCode);
+//      }
+
+      String serverPath = serverName2;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+
+       pir = digitalRead(pirPin);
+  vib = !digitalRead(vibPin);
+   vibReadings[i] = vib;
+    changesVib[i] = abs(vibReadings[i+1]-vibReadings[i]);
+    i++;
+    if (i == 10) {i = 0;}
+
+    sum = 0;
+    for(int j = 0; j < 9; j++) {
+      sum += changesVib[j];
+    }
+
+    if(sum > 3) {
+    digitalWrite(buzzerPin, HIGH);
+  }
+  else {
+    digitalWrite(buzzerPin,LOW);
+  }
+
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+
+       pir = digitalRead(pirPin);
+  vib = !digitalRead(vibPin);
+   vibReadings[i] = vib;
+    changesVib[i] = abs(vibReadings[i+1]-vibReadings[i]);
+    i++;
+    if (i == 10) {i = 0;}
+
+    sum = 0;
+    for(int j = 0; j < 9; j++) {
+      sum += changesVib[j];
+    }
+
+    if(sum > 3) {
+    digitalWrite(buzzerPin, HIGH);
+  }
+  else {
+    digitalWrite(buzzerPin,LOW);
+  }
+
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+
+      
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+
+void setupAccelSensor(void) {
+  pinMode(axPin,INPUT);
+  pinMode(ayPin,INPUT);
+  pinMode(azPin,INPUT);
+}
+
+
+void readAccelSensor(void) {
+  ax = analogRead(axPin);
+  ay = analogRead(ayPin);
+  az = analogRead(azPin);
+}
+
+//
+//void setupGPSSensor(void) {
+//  gpsSerial.begin(9600); // connect gps sensor
+//}
+//
+//void readGPSData(void) {
+//  float lat = 0.0,lon = 0.0; 
+//  while(gpsSerial.available()){ // check for gps data
+//    if(gps.encode(gpsSerial.read())) { 
+//    gps.f_get_position(&lat,&lon); // get latitude and longitude
+//   }
+//  }
+//
+// latitude = String(lat,6);
+// longitude = String(lon,6);
+// Serial.println(latitude);
+// Serial.println(longitude);
+//}
--- README.md
+++ README.md
@@ -5,4 +5,18 @@ ___
 
 # ECE 140B: CarProtect
 
-This repository contains all the code files necessary for our project.
+Hello! This repository contains all the code files for our project. The complete demo video can be found at: https://youtu.be/MSkuZj5Wf60 
+
+___
+
+## How the Code is Organized + Instructions to Run the Code
+
+The __ESP 32__ folder contains the Arduino code that needs to be uploaded to the board. The *CameraWebServerGirish* code is not used in this project per se, but is a useful tutorial on using the ESP32 cam, that inspired us to build the rest of the project.
+
+Inside the __ESP 32__ folder, you can find the __CamUpload__ folder, which contains the code that needs to be uploaded to the ESP32 Cam. The __CarProtect_ESP32__ folder contains the code that needs to be uploaded to the ESP32 Wrover Module board.
+
+Once the code is uploaded to each board, remove all connections to the ESP32 cam. Then, connect the 5V pin of the ESP32 cam to the 5V-OUT pin of the ESP32 Wrover Module, and connect the ground pin of the ESP32 cam to the ground pin of the Wrover module. Then, connect the ESP32 Wrover module to a power source.
+
+The __web__ folder contains the code necessary for the web server and database used for our full-stack IoT project. This involves the basic server architecture we learned in ECE 140A, and we have a Docker container with the web server + MySQL database. This is hosted via DigitalOcean.
+
+Link: http://164.92.89.132/
